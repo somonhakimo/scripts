c928b21e828581576c7627e3d2a49e00cdbc8fcb
somon <hasomon95@gmail.com>
2025-06-03 11:42:05 -0500

    adding workflow file that will automate my work haha

---
+++ myscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello from my script!"
+echo "Listing files in the repository:"
+ls -F
+echo "Script finished!"
+
--- session7/message_slack.sh
+++ session7/message_slack.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+url="https://hooks.slack.com/services/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"
+
+## Example 1:
+curl -X POST $url \
+  -H 'Content-type: application/json' \
+  -d '{"text": "Hello Everyone"}'
